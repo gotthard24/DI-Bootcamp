f7393bb2a19350242779ecf93cc976d619adad63
Daniel <danya.smile@gmail.com>
2024-03-06 18:37:26 +0200

    few func tic tac added

--- Week2/Day5/tic_tac_toe.py
+++ Week2/Day5/tic_tac_toe.py
@@ -1,6 +1,18 @@
 import msvcrt
 import sys
+import os
 
+def swap(is_x):
+    if is_x == True:
+        is_x = False
+    else:
+        is_x = True    
+    return is_x    
+        
+def clear_screen():
+    os.system('cls' if os.name == 'nt' else 'clear')
+    
+    
 def draw_map(game_map, map_size):
     for i in range(len(game_map)):
         for j in range(map_size):
@@ -11,13 +23,13 @@ def draw_map(game_map, map_size):
 def get_key_press():
     while True:
         key = msvcrt.getch()
-        if key == b'w' or key == b'W' or key == b'\xe0H':
+        if key == b'w' or key == b'W':
             return 'Up'
-        elif key == b's' or key == b'S' or key == b'\xe0P':
+        elif key == b's' or key == b'S':
             return 'Down'
-        elif key == b'a' or key == b'A' or key == b'\xe0K':
+        elif key == b'a' or key == b'A':
             return 'Left'
-        elif key == b'd' or key == b'D' or key == b'\xe0M':
+        elif key == b'd' or key == b'D':
             return 'Right'
         elif key == b'\r':
             return True
@@ -39,78 +51,88 @@ def move_cursor_right(chars):
     sys.stdout.flush()
 
 
-def draw_existing(positions):
+def draw_existing(positions, is_x):
     for i in range(len(positions)):
         position = positions[i]
-        draw_x(position[0], position[1])
+        place_x(position[0], position[1])
 
 
-def draw_x(y, x, color = '\033[0m'):
-    position = [y, x]
+def draw_x(up, right, color = '\033[0m'):
+    move_cursor_up(up)
+    move_cursor_right(right)        
+    sys.stdout.write(color + '\bX\b' + '\033[0m') 
+    sys.stdout.flush()
+    move_cursor_down(up)
+    move_cursor_left(right)
+    
+    
+def draw_o(up, right,color = '\033[0m'):
+    move_cursor_up(up)
+    move_cursor_right(right)        
+    sys.stdout.write(color + '\bO\b' + '\033[0m') 
+    sys.stdout.flush()
+    move_cursor_down(up)
+    move_cursor_left(right)
+    
+
+def place_x(y, x, color = '\033[0m'):  
+    up_0 = 7
+    up_1 = 5
+    up_2 = 3
+    right_0 = 3
+    right_1 = 9
+    right_2 = 14
+    
+    position = [y,x]
     
     if position == [0,0]:
-        move_cursor_up(7)
-        move_cursor_right(3)
-        sys.stdout.write(color + '\bX\b' + '\033[0m') 
-        sys.stdout.flush()
-        move_cursor_down(7)
-        move_cursor_left(3)
+        draw_x(up_0,right_0,color)
     if position == [0,1]:
-        move_cursor_up(7)
-        move_cursor_right(9)
-        sys.stdout.write(color + '\bX\b' + '\033[0m') 
-        sys.stdout.flush()
-        move_cursor_down(7)
-        move_cursor_left(9)
+        draw_x(up_0,right_1,color)
     if position == [0,2]:
-        move_cursor_up(7)
-        move_cursor_right(14)
-        sys.stdout.write(color + '\bX\b' + '\033[0m') 
-        sys.stdout.flush()
-        move_cursor_down(7)
-        move_cursor_left(14)
+        draw_x(up_0,right_2,color)
     if position == [1,0]:
-        move_cursor_up(5)
-        move_cursor_right(3)
-        sys.stdout.write(color + '\bX\b' + '\033[0m') 
-        sys.stdout.flush()
-        move_cursor_down(5)
-        move_cursor_left(3)
+        draw_x(up_1,right_0,color)
     if position == [1,1]:
-        move_cursor_up(5)
-        move_cursor_right(9)
-        sys.stdout.write(color + '\bX\b' + '\033[0m') 
-        sys.stdout.flush()
-        move_cursor_down(5)
-        move_cursor_left(9)
+        draw_x(up_1,right_1,color)
     if position == [1,2]:
-        move_cursor_up(5)
-        move_cursor_right(14)
-        sys.stdout.write(color + '\bX\b' + '\033[0m') 
-        sys.stdout.flush()
-        move_cursor_down(5)
-        move_cursor_left(14)
+        draw_x(up_1,right_2,color)
     if position == [2,0]:
-        move_cursor_up(3)
-        move_cursor_right(3)
-        sys.stdout.write(color + '\bX\b' + '\033[0m') 
-        sys.stdout.flush()
-        move_cursor_down(3)
-        move_cursor_left(3)
+        draw_x(up_2,right_0,color)
     if position == [2,1]:
-        move_cursor_up(3)
-        move_cursor_right(9)
-        sys.stdout.write(color + '\bX\b' + '\033[0m') 
-        sys.stdout.flush()
-        move_cursor_down(3)
-        move_cursor_left(9)
+        draw_x(up_2,right_1,color)
     if position == [2,2]:
-        move_cursor_up(3)
-        move_cursor_right(14)
-        sys.stdout.write(color + '\bX\b' + '\033[0m') 
-        sys.stdout.flush()
-        move_cursor_down(3)
-        move_cursor_left(14)
+        draw_x(up_2,right_2,color)
+        
+    
+def place_o(y, x, color = '\033[0m'):
+    up_0 = 7
+    up_1 = 5
+    up_2 = 3
+    right_0 = 3
+    right_1 = 9
+    right_2 = 14
+    
+    position = [y,x]
+    
+    if position == [0,0]:
+        draw_o(up_0,right_0,color)
+    if position == [0,1]:
+        draw_o(up_0,right_1,color)
+    if position == [0,2]:
+        draw_o(up_0,right_2,color)
+    if position == [1,0]:
+        draw_o(up_1,right_0,color)
+    if position == [1,1]:
+        draw_o(up_1,right_1,color)
+    if position == [1,2]:
+        draw_o(up_1,right_2,color)
+    if position == [2,0]:
+        draw_o(up_2,right_0,color)
+    if position == [2,1]:
+        draw_o(up_2,right_1,color)
+    if position == [2,2]:
+        draw_o(up_2,right_2,color)
     
         
 
@@ -136,6 +158,8 @@ x = 1
 position = coordinates[y][x]
 positions = []
 is_playing = False
+is_x = True
+red = '\033[91m'
 
 draw_map(game_map, map_size)
 
@@ -150,9 +174,13 @@ while not is_playing:
         is_playing = True
         
 while is_playing:  
+    clear_screen()
     draw_map(game_map, map_size) 
-    draw_existing(positions)
-    draw_x(y, x, '\033[91m')
+    draw_existing(positions, is_x)
+    if is_x == True:
+        place_x(y, x, red)
+    else:
+        place_o(y, x, red)
     key = get_key_press()
     
     if key == False:
@@ -174,4 +202,5 @@ while is_playing:
         if x > 2:
             x = 0
     if key == True:
-        positions.append([y,x])
+        positions.append([y,x]) #is_x
+        is_x = swap(is_x)

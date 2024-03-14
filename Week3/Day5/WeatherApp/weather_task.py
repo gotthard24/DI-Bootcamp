ab55f19344dcb53bec5b74510f26ca6d9b25465a
Daniel <danya.smile@gmail.com>
2024-03-14 20:38:07 +0200

    week3 day5 weather added

--- Week3/Day5/WeatherApp/weather_task.py
+++ Week3/Day5/WeatherApp/weather_task.py
@@ -1,16 +1,37 @@
 from pyowm.owm import OWM
-owm = OWM('91162d31c7f79579eac79dda7e69099d')
-reg = owm.city_id_registry()
+from datetime import datetime, timezone
+import json
+import dk
+
+API_KEY = '91162d31c7f79579eac79dda7e69099d'
+owm = OWM(API_KEY)
 weather_mgr = owm.weather_manager()
-observation = weather_mgr.weather_at_place('Tel aviv,IL')
+
+with open("Week3\Day5\WeatherApp\city.list.json", "r", encoding="utf-8") as file:
+    id_list = json.load(file)
+    
+user_input = dk.is_int(input("Please enter ID:\n\nLondon - 1006984\nMoscow - 524894\nTel-Aviv - 293396\n> "))
+
+for i in range(len(id_list)):
+    if id_list[i]['id'] == user_input:
+        city = id_list[i]['name']
+        country = id_list[i]['country']
+        city_id = f"{city},{country}"
+        break
+
+observation = weather_mgr.weather_at_place(city_id)
 weather = observation.weather
+
+print(city_id)
 print()
-print(weather)
-temp_dict_celsius = weather.temperature('celsius')
-print(temp_dict_celsius)
-wind_dict_in_meters_per_sec = observation.weather.wind()
-print(wind_dict_in_meters_per_sec)
-sunrise_date = weather.sunrise_time(timeformat='date')
-print(sunrise_date)
-sunrset_date = weather.sunset_time(timeformat='date')
-print(sunrset_date)
+print(f"Current weather at {city}:{country}:")
+print("Status:", weather.detailed_status)
+print("Temperature:", weather.temperature('celsius')['temp'], "°C")
+
+wind_speed = weather.wind()['speed']
+print("Wind speed:", wind_speed, "m/s")
+
+sunrise_time = datetime.fromtimestamp(weather.sunrise_time(), timezone.utc).strftime('%Y-%m-%d %H:%M:%S')
+sunset_time = datetime.fromtimestamp(weather.sunset_time(), timezone.utc).strftime('%Y-%m-%d %H:%M:%S')
+print("Sunrise time:", sunrise_time)
+print("Sunset time:", sunset_time)

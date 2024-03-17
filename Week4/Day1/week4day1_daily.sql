00860b16ba0deaabcf03ba6039d0809e17573e91
Daniel <danya.smile@gmail.com>
2024-03-17 13:30:15 +0200

    week4 day1 daily added

---
+++ Week4/Day1/week4day1_daily.sql
@@ -0,0 +1,7 @@
+-- SELECT * FROM actors;
+-- SELECT COUNT(*) FROM actors;
+
+-- INSERT INTO actors (first_name)
+-- VALUES ('Sasha');
+
+-- the NULL value in the "last_name" column of the "actors" relationship violates the NOT NULL constraint

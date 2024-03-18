a50bdc4457c927b8adc3582ad2d5121a2a1e9511
Daniel <danya.smile@gmail.com>
2024-03-18 12:10:15 +0200

    week4 day2 xp daily added

---
+++ Week4/Day2/week4day2_xp.sql
@@ -0,0 +1,49 @@
+-- EXERCISE 1
+-- SELECT * FROM items ORDER BY price ASC;
+-- SELECT * FROM items WHERE price >= 80 ORDER BY price DESC;
+-- SELECT first_name, last_name FROM customers ORDER BY first_name ASC LIMIT 3;
+-- SELECT last_name FROM customers ORDER BY last_name DESC;
+
+-- EXERCISE 2
+-- SELECT * FROM customer;
+-- SELECT first_name || ' ' || last_name AS full_name FROM customer;
+-- SELECT DISTINCT create_date FROM customer;
+-- SELECT * FROM customer ORDER BY first_name DESC;
+-- SELECT film_id,title,description,release_year,rental_rate FROM film 
+-- ORDER BY rental_rate ASC;
+-- SELECT address, phone FROM address WHERE district = 'Texas';
+-- SELECT * FROM film WHERE film_id = 15 OR film_id = 150;
+-- SELECT film_id, title, description, length, rental_rate FROM film 
+-- WHERE title ILIKE '%titanic%';
+-- SELECT film_id, title, description, length, rental_rate FROM film 
+-- WHERE title ILIKE 'ti%';
+-- SELECT * FROM film ORDER BY replacement_cost ASC LIMIT 10;
+-- SELECT * FROM film ORDER BY replacement_cost ASC LIMIT 10 OFFSET 10;
+-- SELECT *
+-- FROM (
+--     SELECT *,
+--            ROW_NUMBER() OVER (ORDER BY replacement_cost ASC) AS row_num
+--     FROM film
+-- ) AS subquery
+-- WHERE row_num BETWEEN 11 AND 20;
+-- SELECT customer.first_name, customer.last_name, payment.amount, payment.payment_date
+-- FROM customer 
+-- INNER JOIN payment ON payment.customer_id = customer.customer_id
+-- ORDER BY customer.customer_id;
+-- SELECT * FROM film LEFT JOIN inventory 
+-- ON inventory.film_id = film.film_id
+-- WHERE inventory.film_id IS NULL;
+-- SELECT city.city|| ' is in ' ||country.country FROM city
+-- JOIN country
+-- ON city.country_id = country.country_id;
+
+-- SELECT 
+-- 	customer.customer_id, 
+-- 	customer.first_name || ' ' ||customer.last_name as full_name,
+-- 	payment.amount,
+-- 	payment.payment_date,
+-- 	staff.staff_id
+-- FROM customer
+-- JOIN payment ON customer.customer_id = payment.customer_id
+-- JOIN staff ON payment.staff_id = staff.staff_id
+-- ORDER BY staff.staff_id

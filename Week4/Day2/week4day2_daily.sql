0d7fe23c8eb5c061a99cba20ea8fce011aa09050
Daniel <danya.smile@gmail.com>
2024-03-18 13:42:08 +0200

    week4 day2 daily added

---
+++ Week4/Day2/week4day2_daily.sql
@@ -0,0 +1,39 @@
+-- CREATE TABLE FirstTab (
+--      id integer, 
+--      name VARCHAR(10)
+-- )
+
+--creates table with two columns(id and name)
+
+-- INSERT INTO FirstTab VALUES
+-- (5,'Pawan'),
+-- (6,'Sharlee'),
+-- (7,'Krish'),
+-- (NULL,'Avtaar')
+
+--adding 4 new rows
+
+-- SELECT * FROM FirstTab
+
+-- outputting all data of the table
+
+-- CREATE TABLE SecondTab (
+--     id integer 
+-- )
+
+--creates table with only one column id
+
+-- INSERT INTO SecondTab VALUES
+-- (5),
+-- (NULL)
+
+--adding 2 new rows to second table
+
+-- SELECT * FROM SecondTab
+
+-- outputting all data of the second table
+
+--Q1 - 0
+--Q2 - 2
+--Q3 - 2
+--Q4 - 2

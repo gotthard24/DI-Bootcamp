a2a1dee64e51e625f5e7b1edf6c013291cdda091
Daniel <danya.smile@gmail.com>
2024-03-25 11:21:10 +0200

    stars added

--- Week5/Daily/daily.js
+++ Week5/Daily/daily.js
@@ -1,15 +1,15 @@
-// let sentence = 'The movie is not that bad, I like it'
-let sentence = 'This movie is not so bad !'
-// let sentence = 'This dinner is bad !' 
-let wordNot = sentence.indexOf('not')
-let wordBad = sentence.indexOf('bad')
+// // let sentence = 'The movie is not that bad, I like it'
+// let sentence = 'This movie is not so bad !'
+// // let sentence = 'This dinner is bad !' 
+// let wordNot = sentence.indexOf('not')
+// let wordBad = sentence.indexOf('bad')
 
-console.log(wordNot)
-console.log(wordBad)
+// console.log(wordNot)
+// console.log(wordBad)
 
-if (wordNot < wordBad)
-{
-    sentence = sentence.replace(/not.*?bad/g, "good")
-}
+// if (wordNot < wordBad)
+// {
+//     sentence = sentence.replace(/not.*?bad/g, "good")
+// }
 
-console.log(sentence)
+// console.log(sentence)
---
+++ Week5/Daily/stars.js
@@ -0,0 +1,6 @@
+let maxLength = 6
+
+for (i = 1; i <= maxLength; i++)
+{
+    console.log('*'.repeat(i))
+}

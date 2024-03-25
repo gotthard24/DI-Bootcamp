04ba52642a796669e8e364830dde5f4e11769502
Daniel <danya.smile@gmail.com>
2024-03-25 11:38:41 +0200

    2nd stars variant

--- Week5/Daily/stars.js
+++ Week5/Daily/stars.js
@@ -1,6 +1,21 @@
 let maxLength = 6
+let maxHeight = 6
 
 for (i = 1; i <= maxLength; i++)
 {
     console.log('*'.repeat(i))
 }
+
+let rows = []
+for (let i = 1; i <= maxHeight; i++) 
+{
+    let row = ''
+    for (let j = 1; j <= i; j++) 
+    {
+        row += '*'
+    }
+    rows.push(row)
+}
+
+for (let row of rows)
+console.log(row)

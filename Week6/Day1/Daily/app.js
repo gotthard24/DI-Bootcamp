e28ca676d020b72d8d177ed5e5a55927e7c62257
Daniel <danya.smile@gmail.com>
2024-03-31 14:31:46 +0300

    week6 day1 daily added

---
+++ Week6/Day1/Daily/app.js
@@ -0,0 +1,34 @@
+let client = "Betty";
+
+const groceries = {
+    fruits : ["pear", "apple", "banana"],
+    vegetables: ["tomatoes", "cucumber", "salad"],
+    totalPrice : "20$",
+    other : {
+        paid : true,
+        meansOfPayment : ["cash", "creditCard"]
+    }
+}
+
+const displayGroceries = (groceries) => {
+    groceries.fruits.forEach(element => {
+        console.log(element)
+    });
+};
+
+const cloneGroceries = () => {
+    let name = client
+    let shopping = {...groceries}
+    shopping.totalPrice = '35$'
+    shopping.other.paid = false
+    return shopping
+}
+
+displayGroceries(groceries)
+
+const shopping = cloneGroceries()
+
+console.log(groceries);
+console.log(shopping);
+
+//All works properly because of this syntax - {...groceries}, so my shopping obj has it,s own place in the memory

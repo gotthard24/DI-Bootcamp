31b6eb5398eb9d743057d46a5a4bd031f6335ba5
Daniel <danya.smile@gmail.com>
2024-04-08 15:14:32 +0300

    Week7 day2 Daily added

---
+++ Week7/Day2/Daily/app.js
@@ -0,0 +1,35 @@
+async function submitHandler(event){
+    event.preventDefault()
+    const root = document.getElementById('root')
+    const api_key = 'hpvZycW22qCjn5cRM1xtWB8NKq4dQ2My';
+    const rating = 'g';
+    const url = `https://api.giphy.com/v1/gifs/random?rating=${rating}&tag=${event.target.tag}&api_key=${api_key}`;
+    
+    try {
+        let response = await fetch(url);
+        if (!response.ok) {
+            throw new Error(`HTTP error! status: ${response.status}`);
+        }
+        let data = await response.json();
+        console.log(data);
+
+        let newDiv = `<div>
+        <img src=${data.data.images.original.url}>
+        <button onclick='deleteHandler(event)'>DELETE GIF</button>
+        </div>`
+        root.innerHTML += newDiv
+    } catch (error) {
+        console.error('Error:', error.message);
+    }
+}
+
+function deleteHandler(event) {
+    event.preventDefault()
+    let div = event.target.parentElement;
+    div.remove()
+}
+
+function deleteAll(event) {
+    event.preventDefault()
+    document.getElementById('root').innerHTML = ''
+}

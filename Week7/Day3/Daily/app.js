c8a399f8ea43e7c26fd6de6a8dab2eaacd7a686d
Daniel <danya.smile@gmail.com>
2024-04-09 15:55:20 +0300

    styling added

--- Week7/Day3/Daily/app.js
+++ Week7/Day3/Daily/app.js
@@ -2,15 +2,45 @@ addToSelect()
 
 async function convert(event) {
     event.preventDefault()
+
+    // let curr1 = document.forms[0].currency1.value.slice(0,3)
+    // let curr2 = document.forms[0].currency2.value.slice(0,3)
+    // let amount = document.forms[0].amount.value
+
+    const api_key = '28b90b480c65969a8af17862'
+    const url = `https://v6.exchangerate-api.com/v6/${api_key}/latest/USD/`
+
+    let data = await getData(url)
+    let result = await calculate(data.conversion_rates)
+    document.forms[0].result.value = result
+}
+
+// async function convert(event) {
+//     event.preventDefault()
+//     let curr1 = document.forms[0].currency1.value.slice(0,3)
+//     let curr2 = document.forms[0].currency2.value.slice(0,3)
+//     let amount = document.forms[0].amount.value
+
+//     const api_key = '28b90b480c65969a8af17862'
+//     const url = `https://v6.exchangerate-api.com/v6/${api_key}/pair/${curr1}/${curr2}/${amount}`
+
+//     data = await getData(url)
+//     document.forms[0].result.value = data.conversion_result
+// }
+
+async function calculate(obj) {
     let curr1 = document.forms[0].currency1.value.slice(0,3)
     let curr2 = document.forms[0].currency2.value.slice(0,3)
     let amount = document.forms[0].amount.value
+    console.log((obj[curr1] / obj[curr2]) * amount);
+    return (obj[curr2] / obj[curr1]) * amount
+}
 
-    const api_key = '28b90b480c65969a8af17862'
-    const url = `https://v6.exchangerate-api.com/v6/${api_key}/pair/${curr1}/${curr2}/${amount}`
-
-    data = await getData(url)
-    document.forms[0].result.value = data.conversion_result
+function swap() {
+    // event.preventDefault()
+    let curr1 = document.forms[0].currency1.value
+    document.forms[0].currency1.value = document.forms[0].currency2.value
+    document.forms[0].currency2.value = curr1
 }
 
 async function getData(url) {

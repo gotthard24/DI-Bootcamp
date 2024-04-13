0ff7ee6acd6f0f39f5d8e4a0742ade8e7b71fcdb
Daniel <danya.smile@gmail.com>
2024-04-13 21:26:50 +0300

    Week7 Day5 daily added

---
+++ Week7/Day5/Daily/app.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const app = express();
+
+app.use(express.json());
+// app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+
+const emojis = [
+    { name: 'grinning face', emoji: '😀' },
+    { name: 'grinning face with big eyes', emoji: '😃' },
+    { name: 'grinning face with smiling eyes', emoji: '😄' },
+    { name: 'beaming face with smiling eyes', emoji: '😁' },
+    { name: 'grinning squinting face', emoji: '😆' },
+    { name: 'grinning face with sweat', emoji: '😅' },
+];
+
+const port = 7777;
+app.listen(port, () => console.log(`Server is running on port ${port}`));
+
+app.get('/emojis', (req, res) => {
+    try {
+        res.json(emojis); 
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'Failed to fetch emojis' });
+    }
+});
---
+++ Week7/Day5/Daily/guess.js
@@ -0,0 +1,79 @@
+let currentQ = 0
+let score = 0
+let emojis
+
+async function getEmojis() {
+    const url = 'http://localhost:7777/emojis';
+    try {
+        const response = await fetch(url);
+        if (!response === 200) {
+            throw new Error(`${response.status}`);
+        }
+        const data = await response.json();
+        console.log(data);
+        return data
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+async function question(num){
+    const section = document.getElementById('output')
+    const data = await getEmojis()
+    let options
+
+    section.textContent = data[num].emoji
+    if (num < data.length/2)
+    {
+        options = [data[0].name,data[1].name,data[2].name,data[3].name]
+    } else{
+        options = [data[2].name,data[3].name,data[4].name,data[5].name]
+    }
+    options = shuffleArray(options)
+
+    document.getElementById(`option1`).textContent = options[0]
+    document.getElementById(`option2`).textContent = options[1]
+    document.getElementById(`option3`).textContent = options[2]
+    document.getElementById(`option4`).textContent = options[3]
+
+    document.getElementById('question').style.display = 'block'
+    emojis = data
+}
+
+function start(event) {
+    event.preventDefault()
+    document.getElementById('startbtn').style.display = 'none';
+    question(currentQ)
+}
+
+function check(event){
+    event.preventDefault()
+    if(event.target.textContent === emojis[currentQ].name){
+        document.getElementById('score').textContent = ++score
+        document.getElementById('output').innerHTML += 'Good Job'
+    } else{
+        document.getElementById('output').innerHTML += 'Wrong answer'
+    }
+
+    if (currentQ < emojis.length- 1){
+        document.getElementById('nextbtn').style.display = 'block';
+    } else {
+        document.getElementById('output').innerHTML = '<h1>THE END</h1>'
+    }
+    document.getElementById('question').style.display = 'none'
+}
+
+function next(event) {
+    event.preventDefault()
+    document.getElementById('nextbtn').style.display = 'none';
+    currentQ ++
+    question(currentQ)
+}
+
+function shuffleArray(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+    }
+    return array;
+}

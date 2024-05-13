a666a68d201e2bf0de7de6782ac39743c6d16148
Daniel <danya.smile@gmail.com>
2024-05-13 16:50:16 +0300

    Week10 day2 xp added

--- Week10/Day2/XP/xp/src/App.jsx
+++ Week10/Day2/XP/xp/src/App.jsx
@@ -1,9 +1,34 @@
+import { useRef } from 'react'
 import './App.css'
+import { addTask, removeTask, completeTask} from './redux/actions'
+import {useSelector, useDispatch} from 'react-redux'
 
 function App() {
+  const tasks = useSelector((state) => state.taskReducer.tasks)
+  const dispatch = useDispatch()
+  const inputRef = useRef()
+
+  const handleAddTask = () =>{
+    dispatch(addTask({name: inputRef.current.value, isCompleted: false}))
+    inputRef.current.value = null
+  }
+
   return (
     <>
-      
+    <h1>To-Do List</h1>
+    <input placeholder='Add task' ref={inputRef}/>
+    <button onClick={handleAddTask}>Add Task</button>
+    {tasks.map((item,i)=>{
+      return(
+        <div key={i}>
+          <span>
+            {item.isCompleted ? <p style={{ textDecoration: 'line-through' }}>{item.name}</p> :<p>{item.name}</p>}
+            <button onClick={() => dispatch(completeTask(i))}>Complete</button> 
+            <button onClick={() => dispatch(removeTask(i))}>Remove</button>
+          </span>
+        </div>
+      )
+    })}
     </>
   )
 }
